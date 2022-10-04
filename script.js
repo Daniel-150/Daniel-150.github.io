1c1f27401802b5a9e4ee5b55287d5b586e19ac41
Daniel-150 <79731555+Daniel-150@users.noreply.github.com>
2022-10-04 09:35:30 -0300

    Municipalidad San Antonio de Areco

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-menu")
+const servicios = document.querySelector(".servicios");
+const menuSecundario = document.querySelector(".menu-secundario")
+const boton = document.querySelector(".boton")
 
 hamburger.addEventListener("click", ()=> {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll(".nav-link").forEach(n => 
-    n.addEventListener("click",() =>{
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }));
+  n.addEventListener("click",() =>{
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+      menuSecundario.classList.remove("menu2") 
+  }));
 
-    window.addEventListener("scroll", ()=> {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    } );
+  window.addEventListener("scroll", ()=> {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+  } );
+
+servicios.addEventListener("click", ()=> {
+ menuSecundario.classList.toggle("menu2");
+ 
+});
+
+document.querySelectorAll(".boton").forEach(n =>
+  n.addEventListener("click",()=>{
+    menuSecundario.classList.remove("menu2")
+  }
+
+  ))
+  document.querySelectorAll(".hamburger").forEach(n =>
+    n.addEventListener("click",()=>{
+      menuSecundario.classList.remove("menu2")
+    }
+  
+    ))
+
+
